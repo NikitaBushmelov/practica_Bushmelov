24b15884ace24d84244d5e272911216b7a88427f
NikitaBushmelov <bushmelovnikita@gmail.com>
2021-12-13 10:43:23 +0100

    Commit1

---
+++ practica_Bushmelov/src/practica_Bushmelov/exercici3.java
@@ -0,0 +1,20 @@
+package practica_Bushmelov;
+
+public class exercici3 {
+
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
+	
